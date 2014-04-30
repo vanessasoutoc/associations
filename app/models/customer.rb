e432ae3e359bdf9e410b96f6656075ae63a1d264
Tim C <uniquelysimilar@yahoo.com>
2014-04-30 13:00:08 -0600

    Implemented customer form state select tag

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,9 +5,9 @@ class Customer < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 30 }
 	validates :street, presence: true, length: { maximum: 40 }
 	validates :city, presence: true, length: { maximum: 30 }
-	validates :state, presence: true	# implement select box
+	validates :state, presence: true, length: { maximum: 20 }
 	validates :zipcode, presence: true, numericality: { only_integer: true }, length: { is: 5 }
 	validates_format_of :home_phone, with: /\d{3}-\d{3}-\d{4}/
 	validates_format_of :work_phone, with: /\d{3}-\d{3}-\d{4}/, allow_blank: true
-	validates :email, presence: true, uniqueness: true # add format validation
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
