cb04d5b2e5744213610cdeb2227590ab1609ed69
Tim <uniquelysimilar@yahoo.com>
2014-06-02 17:35:53 -0600

    Implementing user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,34 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User '#{@user.name}' created successfully"
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+#    @user = User.find(params[:id])
+#    if @user.update_attributes(user_params)
+#      flash[:success] = "User updated"
+#      redirect_to @user
+#    else
+      render "edit"
+#    end
   end
 
   def destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
