b682b817cd3a113d768afcf020c75f16f5d8c4f7
Tim <uniquelysimilar@yahoo.com>
2014-06-28 11:37:44 -0600

    implemented order update

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,9 @@ class Order < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :date, presence: true
 	validates :total, presence: true,
-										numericality: { greater_than_or_equal_to: 0 }
-										#, format: { with: /\A(\d+)(\.)?\d{0,2}?\z }
+										numericality: { greater_than_or_equal_to: 0 },
+										format: { with: /\A\d+(\.\d{1,2})?\z/i }
+										# TODO: Format below should handle commas, but doesn't.  Research.
+										# Currency amount (cents optional) Optional thousands separators; optional two-digit fraction
+										#format: { with: /\A[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\z/i }
 end
