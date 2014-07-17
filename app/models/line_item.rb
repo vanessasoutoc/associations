fdf272e0978ca0c7bf03988378881afa69a565eb
Tim <uniquelysimilar@yahoo.com>
2014-07-17 16:16:15 -0600

    removed 'total' from Order add and edit, as it is now calculated

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,7 +3,6 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
 
   self.primary_key = :order_id, :product_id
-#  self.primary_key = [:order_id, :product_id]
 
   def price
   	price = product.unit_price * quantity * (1 - (discount_percent / 100))
