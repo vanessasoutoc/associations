8723ee61821d2cef096278022e47bd65e51a4775
Tim <uniquelysimilar@yahoo.com>
2014-08-04 13:10:14 -0600

    route user to Customer index view after signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             sign_in user
-            redirect_to root_url
+            redirect_to customers_path
         else
             flash.now[:danger] = "Invalid email/password combination"
             render "new"
