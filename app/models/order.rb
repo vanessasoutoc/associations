3d3d23e258291ba9e35fcfc92d68f5e5075b6e29
Tim <uniquelysimilar@yahoo.com>
2014-08-26 19:01:30 -0600

    modified validation error message locations in add/edit views

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,10 @@ class Order < ActiveRecord::Base
 	# Validations
 	validates :description, presence: true, length: { maximum: 50 }
 	validates :date, presence: true
-	validates :total, presence: true,
-										numericality: { greater_than_or_equal_to: 0 },
-										format: { with: /\A\d+(\.\d{1,2})?\z/i }
+	# Since the total is now calculated, this validation is no longer necessary
+	#validates :total, presence: true,
+	#									numericality: { greater_than_or_equal_to: 0 },
+	#									format: { with: /\A\d+(\.\d{1,2})?\z/i }
 										# TODO: Format below should handle commas, but doesn't.  Research.
 										# Currency amount (cents optional) Optional thousands separators; optional two-digit fraction
 										#format: { with: /\A[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?\z/i }
